14866e72d6560f41ffc14357ef430cf84bb3397c
Hein Htet <hhaung15ep@ytu.edu.mm>
2022-02-17 23:17:42 +0630

    Crude index

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $("#classroom_carousel").carousel( { interval: 2000 } );
+    $("#classroom_carousel").carousel( { interval: 10000 } );
     $("#classroom_carousel_button").click(function(){
         if ($("#classroom_carousel_button").find("span").hasClass('fa-pause')) {
             $("#classroom_carousel").carousel('pause');
@@ -12,18 +12,5 @@ $(document).ready(function(){
             $("#classroom_carousel_button").find("span").addClass('fa-pause');                    
         }
     });
-    $("#feedback_carousel").carousel( { data-interval: false } );
-    $("#feedback_carousel_button").click(function(){
-        if ($("#feedback_carousel_button").find("span").hasClass('fa-pause')) {
-            $("#feedback_carousel").carousel('pause');
-            $("#feedback_carousel_button").find("span").removeClass('fa-pause');
-            $("#feedback_carousel_button").find("span").addClass('fa-play');
-        }
-        else if ($("#feedback_carousel_button").find("span").hasClass('fa-play')){
-            $("#feedback_carousel").carousel('cycle');
-            $("#feedback_carousel_button").find("span").removeClass('fa-play');
-            $("#feedback_carousel_button").find("span").addClass('fa-pause');                    
-        }
-    });
-
+ 
 });
